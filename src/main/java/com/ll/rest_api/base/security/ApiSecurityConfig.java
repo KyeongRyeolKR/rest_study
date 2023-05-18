f9c26548d86b7e8212ad7524ca86e41e7014e609
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-05-18 23:58:55 +0900

    admin 아이디에 ADMIN 권한 부여

--- src/main/java/com/ll/rest_api/base/security/ApiSecurityConfig.java
+++ src/main/java/com/ll/rest_api/base/security/ApiSecurityConfig.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -25,7 +23,7 @@ public class ApiSecurityConfig {
     @Bean
     public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .securityMatcher("/api/**")
+                .securityMatcher("/api/**") // 아래의 모든 설정은 /api/** 경로에만 적용
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(authenticationEntryPoint)
                 )
@@ -34,11 +32,11 @@ public class ApiSecurityConfig {
                                 .requestMatchers(HttpMethod.POST, "/api/*/member/login").permitAll() // 로그인은 누구나 가능
                                 .requestMatchers(HttpMethod.GET, "/api/*/articles").permitAll() // 글 보기는 누구나 가능
                                 .requestMatchers(HttpMethod.GET, "/api/*/articles/*").permitAll() // 글 보기는 누구나 가능
-                                .anyRequest().authenticated()  // 나머지는 로그인이 되어있어야 함
+                                .anyRequest().authenticated() // 나머지는 인증된 사용자만 가능
                 )
                 .cors().disable() // 타 도메인에서 API 호출 가능
                 .csrf().disable() // CSRF 토큰 끄기
-                .httpBasic().disable() // httpBaic 로그인 방식 끄기
+                .httpBasic().disable() // httpBasic 로그인 방식 끄기
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
@@ -47,7 +45,6 @@ public class ApiSecurityConfig {
                         jwtAuthorizationFilter, // 엑세스 토큰으로 부터 로그인 처리
                         UsernamePasswordAuthenticationFilter.class
                 );
-
         return http.build();
     }
 }
